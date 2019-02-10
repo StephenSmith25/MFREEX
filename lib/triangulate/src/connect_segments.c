3d20cf69736ee5a97a98a78d061d194c861cf88d
ssmith@github.com <ssmith54@qub.ac.uk>
2019-02-10 16:25:08 +0000

    fixing c99 error

--- lib/triangulate/src/connect_segments.c
+++ lib/triangulate/src/connect_segments.c
@@ -9,7 +9,7 @@ int connect_segments(int * segments, int ** connected_segments, int num_segments
 	// loop over segments
 	int * connect_segments = malloc(num_segments*2*sizeof(int));
 
-
+	int i,k;
 
 	int * segments_copy = malloc(2*num_segments*sizeof(int));
 	memcpy(segments_copy,segments,2*num_segments*sizeof(int));
@@ -22,14 +22,14 @@ int connect_segments(int * segments, int ** connected_segments, int num_segments
 	segments_copy[0] = -1;
 	segments_copy[1] = -1;
 
-	for (int i = 1; i < num_segments  ; ++i)
+	for (i = 1; i < num_segments  ; ++i)
 	{
 		connect_segments[2*i] = connect_segments[2*(i-1) + 1];
 		connect_segments[2*i +1 ] = -1 ;
 		int endpoint = connect_segments[2*i];
 
 		// find the endpoint in the list of remaining segments
-		for ( int k = 0 ; k < num_segments ; ++k)
+		for (  k = 0 ; k < num_segments ; ++k)
 		{
 			if ( endpoint == segments_copy[2*k] )
 			{
@@ -41,7 +41,7 @@ int connect_segments(int * segments, int ** connected_segments, int num_segments
 		}
 		if ( connect_segments[2*i+1 ] == -1 )
 		{
-			for (int k = 0; k < num_segments; ++k)
+			for (k = 0; k < num_segments; ++k)
 			{
 				if ( endpoint == segments_copy[2*k+1] )
 				{
